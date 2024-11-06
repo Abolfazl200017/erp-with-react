f5f24f2c7b2a54d393f48a134ed2365e84a106c9
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-06 16:11:08 +0330

    convrt styles with mui on category skeleton

--- src/components/Sidebar/CategorySkeleton.tsx
+++ src/components/Sidebar/CategorySkeleton.tsx
@@ -1,12 +1,12 @@
-import { Skeleton } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
 
 function CategorySkeleton() {
   return (
-    <div className="w-full mt-3 px-2 py-3">
+    <Box sx={{ width: 1, mt: 3, px: 2, py: 3 }}>
       {[1, 2, 3].map((i) => (
         <Skeleton key={i} variant="rounded" height={40} sx={{ marginBottom: '15px' }} />
       ))}
-    </div>
+    </Box>
   );
 }
 
