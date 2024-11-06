aea89e92602849609963ede2528f0189ca79df96
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-06 14:49:51 +0330

    update RouterConfig

--- src/navigation/RouterConfig.tsx
+++ src/navigation/RouterConfig.tsx
@@ -1,21 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import { HOME, SERVER_ERROR } from './CONSTANT';
-import MainLayout from 'layouts/MainLayout';
 import ServerError from 'pages/error/ServerError';
 import NotFound from 'pages/error/NotFound';
 import Home from 'pages/Home';
 import CategoryTasks from 'pages/CategoryTasks';
+import MainLayout from 'layouts/MainLayout';
 
 export const RouterConfig = () => {
-
   return (
     <Routes>
-      <Route path='/' element={<MainLayout />}>
-          <Route path={HOME} element={<Home />} />
-          <Route path="category/:name" element={<CategoryTasks />} />
-        </Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route path={HOME} element={<Home />} />
+        <Route path="category/:name" element={<CategoryTasks />} />
+        <Route path="/users" element={<div>users list works!</div>} />
+        <Route path="/users/:id" element={<div>single user worksdd</div>} />
+        <Route path="/article" element={<div>articles list works!</div>} />
+      </Route>
       <Route path={SERVER_ERROR} element={<ServerError />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
