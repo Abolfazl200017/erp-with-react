060c818604340642cd2f567be98464ece2c3e527
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-11-10 10:20:32 +0330

    remove duplicate rtl cache

--- src/styles/rtlCache.ts
+++
@@ -1,8 +0,0 @@
-import createCache from '@emotion/cache'
-
-const rtlCache = createCache({
-  key: 'mui-rtl',
-  stylisPlugins: [(stylis) => (stylis === 'ltr' ? undefined : stylis.use(require('stylis-plugin-rtl')()))],
-});
-
-export default rtlCache;
